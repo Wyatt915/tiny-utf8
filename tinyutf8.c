03a1419938cd8c4c1f40dd36b5c74f22869ec547
Wyatt Sheffield <cws@wyatts.xyz>
2019-07-09 20:13:30 -0500

    moved algorithm into its own function

--- tinyutf8.c
+++ tinyutf8.c
@@ -2,43 +2,45 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int main(int argc, char** argv){
-    char* input = malloc(10*sizeof(char));
-    fgets(input, 10, stdin);
-    uint32_t val = strtol(input, NULL, 16);
-    free(input);
-    input = NULL;
-
-    int numbytes = 0;
-
+int int_to_utf8(uint32_t val){
+    int width = 0; //The width of the output character in bytes.
     if (val < 0x80){
         //trivial case - this is just ASCII. We're done.
         putchar(val);
         return 0;
     }
-    else if (val < 0x00000800)  numbytes = 2;
-    else if (val < 0x00010000)  numbytes = 3;
-    else if (val < 0x00200000)  numbytes = 4;
-    else if (val < 0x04000000)  numbytes = 5;
-    else if (val < 0x80000000)  numbytes = 6;
+    else if (val < 0x00000800)  width = 2;
+    else if (val < 0x00010000)  width = 3;
+    else if (val < 0x00200000)  width = 4;
+    else if (val < 0x04000000)  width = 5;
+    else if (val < 0x80000000)  width = 6;
     else{
         fprintf(stderr, "Illegal codepoint.\n");
         return 1;
     }
 
-    uint8_t firstbits = 7 - numbytes;
-    uint8_t totalbits = firstbits + (numbytes - 1) * 6;
-    uint8_t msb_mask = 0xff & ~((1 << (firstbits + 1)) - 1);
+    uint8_t firstbits = 7 - width;
+    uint8_t totalbits = firstbits + (width - 1) * 6;
+    //This bitwise hacking produces a byte with a number of leading 1s equal to the value <width>.
+    //For example, if width == 5, then msb_mask will be 0b11111000.
+    uint8_t msb_mask = ~((1 << (firstbits + 1)) - 1);
 
-    //Do the weird case of the first byte...
+    //The first byte is the leading 1s defined by msb_mask followed by the first few bits of <val>.
     putchar(msb_mask | (val >> (totalbits - firstbits)));
-    //now the rest of the bytes are the same.
+    //The remaining bytes are prefixed with 0b10 and contain 6 bits of data.
     char byte;
-    for (int i = numbytes - 2; i >= 0; i--){
+    for (int i = width - 2; i >= 0; i--){
         byte = 0x3f & (val >> 6 * i); //0x3f isolates the six least significant bits
         byte |= 0x80;
         putchar(byte);
     }
-
     return 0;
+}
+
+int main(int argc, char** argv){
+    char* input = malloc(10*sizeof(char));
+    fgets(input, 10, stdin);
+    uint32_t val = strtol(input, NULL, 16);
+    free(input); input = NULL;
+    return int_to_utf8(val);
 }
