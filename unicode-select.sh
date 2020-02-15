416b7cc0f611741a52338370e1fb2d7e03d8fc7b
Wyatt Sheffield <cws@wyatts.xyz>
2020-02-15 11:40:25 -0600

    Fix script to match README

--- unicode-select.sh
+++ unicode-select.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ulist=~/scripts/UnicodeNamesList.txt
-grep -E '^[[:alnum:]]' $ulist | grep -v '<control>' | dmenu -i\
-    | (read selection; grep "$selection" $ulist) | awk '{print $1}'\
-    | tiny-utf8 | xclip -selection "clipboard"
+grep -E '^[[:alnum:]]' $ulist | grep -v '<control>' \
+    | dmenu -i\
+    | (read selection; grep "$selection" $ulist) \
+    | awk '{print $1}'\
+    | tiny-utf8 \
+#   | xclip -selection "clipboard"
+    | xargs xdotool type
