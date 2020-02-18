a910ed76e01a2c71879feb05bc05b65ac99bef3a
Wyatt Sheffield <cws@wyatts.xyz>
2020-02-17 20:26:35 -0600

    Gooder comments

--- tinyutf8.c
+++ tinyutf8.c
@@ -8,9 +8,8 @@
 #include <stdint.h>
 
 int int_to_utf8(uint32_t val){
-    uint8_t width; //The width of the output character in bytes.
+    //trivial case - this is just ASCII. We're done.
     if (val < 0x80){
-        //trivial case - this is just ASCII. We're done.
         putchar(val);
         return 0;
     }
@@ -18,15 +17,22 @@ int int_to_utf8(uint32_t val){
         fprintf(stderr, "Illegal codepoint.\n");
         return 1;
     }
+    // The width of the output character in bytes can be computed with a bunch of if...else
+    // statements. However, as you can see, there is a definite pattern to how the width is
+    // determined based on the codepoint. That is, one more byte is required for each reduction of
+    // leading zeroes by 5 in the binary representation of the codepoint.
     //else if (val < 0x00000800)  width = 2; // 20 leading zeroes
     //else if (val < 0x00010000)  width = 3; // 15 leading zeroes
     //else if (val < 0x00200000)  width = 4; // 10 leading zeroes
     //else if (val < 0x04000000)  width = 5; // 5  leading zeroes
     //else if (val < 0x80000000)  width = 6; // 0  leading zeroes
     // This means that the width is 6 - (leadingzeroes-1/5)
-    width = 6 - ((__builtin_clz(val)-1)/5);
+    // Lots of architectures have instructions for counting leading zeroes, and they can be accessed
+    // by the gcc builtin function __builtin_clz (also supported by clang).
+    uint8_t  width = 6 - ((__builtin_clz(val)-1)/5);
 
     uint8_t firstbits = 7 - width;
+    // Each remaining byte starts with '10' and contains 6 bits of character data
     uint8_t totalbits = firstbits + (width - 1) * 6;
     //This bitwise hacking produces a byte with a number of leading 1s equal to the value <width>.
     //For example, if width == 5, then msb_mask will be 0b11111000.
@@ -35,10 +41,9 @@ int int_to_utf8(uint32_t val){
     //The first byte is the leading 1s defined by msb_mask followed by the first few bits of <val>.
     putchar(msb_mask | (val >> (totalbits - firstbits)));
     //The remaining bytes are prefixed with 0b10 and contain 6 bits of data.
-    char byte;
     for (int i = width - 2; i >= 0; i--){
-        byte = (0x3f & (val >> 6 * i)) | 0x80; //0x3f isolates the six least significant bits
-        putchar(byte);
+        //0x3f isolates the six least significant bits, 0x80 makes each byte start with '10'
+        putchar((0x3f & (val >> 6 * i)) | 0x80);
     }
     return 0;
 }
